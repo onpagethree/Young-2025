728ef48cb3586e24e1c243580495b21c4915d102
onpagethree <140528669+onpagethree@users.noreply.github.com>
2025-10-05 01:37:56 +0900

    11

--- style/gallery/00_flickity.pkgd.js
+++ style/gallery/00_flickity.pkgd.js
@@ -1729,15 +1729,6 @@
 				_this.animate();
 			});
 		}
-
-		/** /
-        // log animation frame rate
-        var now = new Date();
-        if ( this.then ) {
-          console.log( ~~( 1000 / (now-this.then)) + 'fps' )
-        }
-        this.then = now;
-        /**/
 	};
 
 	var transformProperty = getStyleProperty("transform");
